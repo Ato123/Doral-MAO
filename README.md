03e680a246da735b985cff2028cb2aa9d29ecde0
Ato123 <alberticoalvarez123@gmail.com>
2020-08-28 10:22:49 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Doral-MAO
+Here you can find the rankings for each student based off of their practice preformance
+edit1
+
+# Overall
+
+<h3> Algebra 1 </h3>
+<h3> Geometry </h3>
+<h3> Algebra 2 </h3>
+<h3> Precalculus </h3>
+<h3> Calculus </h3>
+
+# Last Practice
+
+<h3> Algebra 1 </h3>
+<h3> Geometry </h3>
+<h3> Algebra 2 </h3>
+<h3> Precalculus </h3>
+<h3> Calculus </h3>
+
---
+++ enter_submissions.py
@@ -0,0 +1,33 @@
+# Complete and working submission maker
+
+import os
+import glob
+
+
+directory = os.path.dirname(os.path.abspath('enter_submissions.py'))
+
+print('format for divisions: alg1, geo, alg2, precal, calc')
+print('What division is being recorded right now?')
+div = input()
+
+print('Clear previous submissions? (y/n)')
+if input().lower().strip() == 'y':
+    for f in glob.glob(directory+'/Moodle_Submissions/'+div+'/*'):
+        os.remove(f)
+
+print('names should be inputted in first-name last-name order')
+print('Inputted answers must be in a single line with blank answers being marked as x')
+print('\nexample input:')
+print('alberto alvarez calc\nabcadacebcacaaebcaebbeacadbdca')
+
+print('type exit to stop the program')
+
+while True:
+    print('Name?')
+    name = input().lower().strip()
+    if name == 'exit':
+        break
+    file = open(directory+'/Moodle_Submissions/'+div+'/'+name+'.txt', 'w')
+    print('Answers?')
+    file.write(input().lower().strip())
+    file.close()
